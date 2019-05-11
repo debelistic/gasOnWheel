8332f96bb9065d52a1f16798e1c28e94ac8f6075
Victor Awotidebe <victorawotidebe@gmail.com>
2019-05-11 22:21:02 +0100

    feat - create landing page
    - add mobile navgations
    - add animations
    - add main page
    [Feature #165851757]

--- ui/js/index.js
+++ ui/js/index.js
@@ -1,1 +1,13 @@
-// alert('connected');
+/* global document */
+const mainNav = document.querySelector('.main_nav');
+const hambugerMenu = document.querySelector('#hambuger_menu');
+
+document.addEventListener('click', (event) => {
+  if (event.target.id === 'hambuger_menu') {
+    mainNav.classList.toggle('open');
+    hambugerMenu.classList.toggle('change');
+  } else {
+    mainNav.classList.remove('open');
+    hambugerMenu.classList.remove('change');
+  }
+});
